ea6018f2d49122a3a531dfb0c4746270198b5518
jeroenlukas <70344836+jeroenlukas@users.noreply.github.com>
2021-06-19 11:36:31 +0200

    Update configMisc.h

--- include/configMisc.h
+++ include/configMisc.h
@@ -1,8 +1,8 @@
 #ifndef CONFIGMISC_H
 #define CONFIGMISC_H
 
-#define CONF_WIFI_SSID  "Rinus 2"
-#define CONF_WIFI_PASSWORD  "scheldestraat"
+#define CONF_WIFI_SSID  "xxx"
+#define CONF_WIFI_PASSWORD  "xxx"
 
 #define POT_HYST    35
 
